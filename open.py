d5cc4333be4fd5d282f631932ae1031b1dbbbc8c
PHÚC NGUYÊN <138844322+PhcNguyen@users.noreply.github.com>
2023-10-15 22:01:06 +0700

    Update open.py

--- open.py
+++ open.py
@@ -3,4 +3,4 @@ try:
   import gspread
 except:
   os.system('pip install gspread')
-os.system('python MGD.PY' if os.name == "nt" else 'python3 MGD.PY')
+os.system('python main.py' if os.name == "nt" else 'python3 main.py') 
